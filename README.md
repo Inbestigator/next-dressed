585ea20bd24f4dfd633b03b5ad662933d0424f8b
Inbestigator <inbestigatoryt@gmail.com>
2025-04-21 02:25:09 -0700

    Update version

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Dressed + NextJS
+# Dressed + Next.js
 
-Surprisingly easier to deploy to Vercel than [deploying a non-framework bot to Vercel](https://github.com/inbestigator/vercel-dressed/)
+## Please see [the economy example bot](https://github.com/inbestigator/dressed-examples/tree/main/node/economy) for a more comprehensive and up-to-date example
+
+Surprisingly easier to deploy to Vercel than a [non-framework bot](https://github.com/inbestigator/vercel-dressed/) on Vercel.
+
--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,21 +1,19 @@
-import { createHandlers, handleRequest } from "@dressed/dressed/server";
+import {
+    handleRequest,
+    setupCommands,
+    setupComponents,
+} from "@dressed/dressed/server";
 import { commandData, componentData } from "@/../bot.gen";
 import { waitUntil } from "@vercel/functions";
 
-const { runCommand, runComponent } = createHandlers(commandData, componentData);
-
 export async function POST(req: Request) {
+    const [runCommand, runComponent] = [
+        setupCommands(commandData),
+        setupComponents(componentData),
+    ] as ((i: unknown) => Promise<void>)[];
     return handleRequest(
-        {
-            headers: {
-                "x-signature-ed25519": req.headers.get("x-signature-ed25519"),
-                "x-signature-timestamp": req.headers.get(
-                    "x-signature-timestamp",
-                ),
-            },
-            text: await req.text(),
-        },
-        (i) => waitUntil(runCommand(i) as Promise<unknown>),
-        (i) => waitUntil(runComponent(i) as Promise<unknown>),
+        req,
+        (i) => waitUntil(runCommand(i)),
+        (i) => waitUntil(runComponent(i)),
     );
 }
